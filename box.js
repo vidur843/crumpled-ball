c02704fba251a704b4f4ca58fcab6b88e280a93f
vidur843 <65580238+vidur843@users.noreply.github.com>
2020-07-01 12:13:28 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,26 @@
+class Box  {
+    constructor(x,y,width,height)
+    {
+        var options=   {
+         restitution:1,
+         density:0.8,
+         friction:0.3,
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);   
+
+    }
+    display()
+    {
+       push();
+       translate(this.body.position.x,this.body.position.y);
+       rotate(this.body.angle);
+        rectMode(CENTER);
+        fill(255);
+        rect(0,0,this.width,this.height);
+        pop();
+    }
+
+}
---
+++ pig.js
@@ -0,0 +1,26 @@
+class Pig              {
+    constructor(x,y)                  {
+
+    var options=     {
+        restitoution:1,
+        density:0.8,
+        friction:0.3
+
+    }
+    this.body=Bodies.rectangle(x,y,50,50,options);
+    this.width=50;
+    this.height=50;
+World.add(world,this.body);
+    }
+display()     {
+    
+    push();
+    translate(this.body.position.x,this.body.position.y);
+    rotate(this.body.angle);
+     rectMode(CENTER);
+     fill("green");
+     rect(0,0,this.width,this.height);
+     pop();
+
+}      
+}
